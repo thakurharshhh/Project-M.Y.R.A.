41f35385dacf2b5906f62853339641243be51b06
HARSH PRATAP SINGH <harshsuryavanshi169@gmail.com>
2025-07-24 10:28:04 +0530

    Update main.py

--- version-1.0/main.py
+++ version-1.0/main.py
@@ -1,1 +1,14 @@
+from gtts import gTTS
+import playsound
+import os
 
+text = "Hello Boss!, Friday Here!"
+
+# Remove previous audio if exists
+if os.path.exists("friday.mp3"): # It will helps to prevent colision of two running command boices
+    os.remove("friday.mp3")
+
+# Use UK English voice (British female style)
+tts = gTTS(text=text, lang='en', tld='co.uk')
+tts.save("friday.mp3")
+playsound.playsound("friday.mp3")
